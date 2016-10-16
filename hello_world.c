1c8db86358b67f5d4aca55c4e45d9a536d798349
dinokiller129 <randomman129@gmail.com>
2016-10-16 00:56:55 -0700

    Update Hello World to add Comments!

--- hello_world.c
+++ hello_world.c
@@ -1,12 +1,24 @@
 /*
-  File_1: Hello World!
-  Language: C
+  File #1: Hello World
+  
+  This program will print "Hello World" and exit.
+*/
+
+//This is a comment
+
+/*
+  This is also a comment
 */
 
 #include <stdio.h>
 
 int main() 
 {
+  /*
+    You can use comments to stop pieces of code from running
+    printf("Don't do drugs kids!");
+  */
+  
   printf("Hello World!");
   
   return 0;
